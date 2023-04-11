f25853a820760b877e7f1cbfea5cd48f499e3ba2
unknown <hibaki1998@gmail.com>
2023-04-11 10:58:48 +0530

    2nd Commit : Diary Entry, Diary Display And Logout

--- index.js
+++ index.js
@@ -1,35 +1,73 @@
+//Importing express and storing it in a variable
 const express = require("express")
-const ds=require("./services/dataService")
 
-const app=express()
+//import ds
+const ds = require("./services/dataService")
 
-// app.post("/login", (req, res) => {
-//     ds.login(req.body.uid, req.body.psw).then(result=>{
-//     res.status(result.statusCode).json(result)
-//     })
-// })
+//import jsonwebtoken
+const jwt = require("jsonwebtoken")
 
 //import cors
 const cors=require("cors")
 
+//App creation
+const app = express()
+
+//integrate app with frontend
 app.use(cors({origin:'http://localhost:4200'}))
+
+//To Covert All Datas From JSON To Js
 app.use(express.json())
+
+//Middleware Creation
+const jwtMiddleware = (req, res, next) => {
+    try {
+        //accessing data from request header
+        const token = req.headers['access_token']
+
+        //verifying the token with secret key
+        const data = jwt.verify(token, "superkey123")
+
+        console.log(data);
+
+        next()
+    }
+    catch {
+        res.status(422).json({
+            status: false,
+            message: "Please Login.",
+            statusCode: 404
+        })
+    }
+}
+
 
 app.post("/register", (req, res) => {
     ds.register(req.body.fname,req.body.lname,  req.body.userid,  req.body.email, req.body.psw).then(result => {
-        res.send(result)
+        res.status(result.statusCode).json(result)
     })
 })
 
 //login - get
 app.post("/login", (req, res) => {
     ds.login(req.body.userid, req.body.psw).then(result=>{
-        res.send(result)
+        res.status(result.statusCode).json(result)
     // res.status(result.statusCode).json(result)
     })
 })
 
+app.post("/diary", jwtMiddleware, (req, res) => {
+    console.log("Server Index : ",req.body.userid, req.body.diaryentry);
+    ds.diary(req.body.userid, req.body.diaryentry).then(result=>{
+        res.status(result.statusCode).json(result)
+    })
+})
 
+app.post("/diaryentries", jwtMiddleware, (req, res) => {
+    ds.getDiary(req.body.userid).then(result=>{
+    res.status(result.statusCode).json(result)
+    })
+})
 app.listen(3000,()=>{
     console.log("server started at port 3000");
 })
--- services/dataService.js
+++ services/dataService.js
@@ -1,67 +1,106 @@
 const db = require('./db')
 const jwt = require("jsonwebtoken")
 
-register = (fname,lname,userid,email, psw) => {
-    today=new Date()
-    //Storing the resolved output of findOne in a variable user, if present :- it returns object and if not present:- it returns null.
-    return db.dduser.findOne({ userid }).then(user => {
-      //if acno is present in db, then oject of that user gets returned else null is returned
-      if (user) {
-        return {
-          status: false,
-          message: "Account Already Present",
-          statusCode: 404
-        }
+register = (fname, lname, userid, email, psw) => {
+  today = new Date()
+  //Storing the resolved output of findOne in a variable user, if present :- it returns object and if not present:- it returns null.
+  return db.dduser.findOne({ userid }).then(user => {
+    //if acno is present in db, then oject of that user gets returned else null is returned
+    if (user) {
+      return {
+        status: false,
+        message: "Account Already Present",
+        statusCode: 404
       }
-      else {
-        newUser = new db.dduser({
-            fname:fname,
-            lname:lname,
-            userid:userid,
-            email:email,
-          password: psw,
-         journal:[]
-        })
-        newUser.save()
-        return {
-          status: true,
-          message: "Account Registered Successfully",
-          statusCode: 200
-        }
+    }
+    else {
+      newUser = new db.dduser({
+        fname: fname,
+        lname: lname,
+        userid: userid,
+        email: email,
+        password: psw,
+        journal: []
+      })
+      newUser.save()
+      return {
+        status: true,
+        message: "Account Registered Successfully",
+        statusCode: 200
       }
-  
-    })
+    }
+
+  })
 }
 
-    login = (userid, psw) => {
+login = (userid, psw) => {
 
-        return db.dduser.findOne({ userid, password: psw }).then(user => {
-          if (user) {
-            //Storing Current User
-            currentuser = user.userid
-            //Token Creation
-            const token = jwt.sign({ userid }, "superkey123")
-            return {
-              status: true,
-              message: "Login Successfull",
-              statusCode: 200,
-              currentuser,
-              token
-            }
-          }
-          else {
-            return {
-              status: false,
-              message: "Incorrect Account No: Or Password",
-              statusCode: 404
-            }
-        }})
-      
+  return db.dduser.findOne({ userid, password: psw }).then(user => {
+    if (user) {
+      //Storing Current User
+      currentuser = user.userid
+      //Token Creation
+      const token = jwt.sign({ userid }, "superkey123")
+      return {
+        status: true,
+        message: "Login Successfull",
+        statusCode: 200,
+        currentuser,
+        token
       }
+    }
+    else {
+      return {
+        status: false,
+        message: "Incorrect Account No: Or Password",
+        statusCode: 404
+      }
+    }
+  })
 
-    
+}
 
-  module.exports={
-    register,
-    login
-  }
+diary = (userid, diaryentry) => {
+  //Converting String Amount To Integer
+  console.log("server fn call : ", userid, diaryentry);
+  today = new Date
+  tdy=today.toDateString()
+  return db.dduser.findOne({ userid }).then(user => {
+    if (user) {
+      user.journal.push({ date: tdy, diaryentry: diaryentry })
+      user.save()
+      return {
+        status: true,
+        message: `Journal Entry Successful`,
+        statusCode: 200
+      }
+    }
+    else {
+      return {
+        status: false,
+        message: "Journal Entry Failed !",
+        statusCode: 402
+      }
+    }
+  })
+}
+
+getDiary = (userid) => {
+  return db.dduser.findOne({userid}).then(user => {
+    if (user) {
+      return {
+        status: true,
+        journal: user.journal,
+        statusCode: 200
+      }
+    }
+  })
+
+}
+
+module.exports = {
+  register,
+  login,
+  diary,
+  getDiary
+}
